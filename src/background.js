17508ed446487819535ab5c08dbb8b9fa722bc4d
bryanbill <emobrya@gmail.com>
2024-01-21 17:14:49 +0300

    adds background tasks

---
+++ src/background.js
@@ -0,0 +1,99 @@
+import { Job, User, Application } from "./models/index.js"
+import { Op } from "sequelize"
+import { CronService, QueueService } from "./services/index.js"
+import chalk from "chalk";
+
+/**
+ * All these tasks will run every minute in the background as a cron job
+ * To add a new task, create a new function and add it to the cron job scheduler
+ */
+
+/**
+ * This task will run every minute and send feedback to employers requesting feedback
+ * for jobs that have ended
+ */
+const feedback = async () => {
+    try {
+        let jobs = await Job.findAll({
+            where: {
+                hasFeedback: false,
+                end: {
+                    [Op.lte]: Date.now()
+                },
+                status: "not-accepting",
+                hasFeedback: false
+            },
+            include: [
+                {
+                    model: User,
+                    as: "user"
+                }
+            ]
+        });
+
+        console.log(chalk.magenta("Found", jobs.length, "jobs"));
+
+        jobs = jobs.map(job => {
+            return {
+                ...job.dataValues,
+                user: job.user.dataValues
+            }
+        });
+
+        jobs.forEach(async (job) => {
+            console.log("Sending feedback for job", job.id);
+            const application = await Application.findOne({
+                where: {
+                    job: job.id,
+                    status: "accepted"
+                },
+                include: [
+                    {
+                        model: User,
+                        as: "user"
+                    }
+                ]
+            });
+            if (!application) return;
+
+            const applicant = application.user.dataValues;
+
+            console.log("Sending feedback to", job.user.email);
+
+            QueueService.queue('email', {
+                to: job.user.email,
+                subject: "Feedback",
+                html: `
+                    <h2>Feedback</h2>
+                    <p>How was your experience with ${applicant.fullName}?</p>
+                    <a href="http://localhost:3000/feedback/${job.id}">Click here to give feedback</a>
+                `,
+                text: `
+                    Feedback
+                    How was your experience with ${applicant.fullName}?
+                    http://localhost:3000/feedback/${job.id}
+                `
+            });
+
+            if (job.user.pushToken)
+                QueueService.queue('notification', {
+                    tokens: [job.user.pushToken],
+                    title: "Feedback",
+                    body: `How was your experience with ${applicant.fullName}?`,
+                })
+
+            await Job.update({ hasFeedback: true }, {
+                where: {
+                    id: job.id
+                }
+            })
+        })
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+new CronService()
+    .addTask(feedback)
+    .schedule();
+
