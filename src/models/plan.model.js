9b4dbf01f8e741400e9fa0d3d3e8688a2ffb6fc9
bryanbill <emobrya@gmail.com>
2024-03-23 18:30:52 +0300

    fix: cast json to text

--- src/models/plan.model.js
+++ src/models/plan.model.js
@@ -1,30 +1,34 @@
 import { database } from "../config/index.js";
 import { DataTypes } from "sequelize";
 
-export const Plan = database.sequelize.define('Plan', {
+export const Plan = database.sequelize.define(
+  "Plan",
+  {
     id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
     },
     name: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     price: {
-        type: DataTypes.INTEGER,
-        allowNull: false
+      type: DataTypes.INTEGER,
+      allowNull: false,
     },
     cycle: {
-        type: DataTypes.ENUM('monthly', 'yearly'),
-        allowNull: false
+      type: DataTypes.ENUM("monthly", "yearly"),
+      allowNull: false,
     },
     features: {
-        type: DataTypes.JSON,
-        allowNull: false,
-        defaultValue: []
-    }
-}, {
-    tableName: 'plans',
-});
+      type: DataTypes.TEXT,
+      allowNull: false,
+      defaultValue: "",
+    },
+  },
+  {
+    tableName: "plans",
+  }
+);
