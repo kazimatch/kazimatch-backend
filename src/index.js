9e850239e13082b5f46abf40adcf6a6389aa4169
bryanbill <emobrya@gmail.com>
2024-06-30 01:14:13 +0300

    disable rate limiter in development

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ const init = async () => {
 
         config.ENV === 'development' ? app.use(morgan('dev')) : null;
 
-        app.use(rateLimit({
+        if (config.ENV === 'production') app.use(rateLimit({
             windowMs: 15 * 60 * 1000,
             max: 100,
             message: "Too many requests from this IP, please try again after 15 minutes",
